d83a72a9524124ab489c73369ebf14fb259aae43
Alexander K <code.alexander.k@gmail.com>
2019-09-13 17:24:30 +0500

    ok

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ The site I used to use finally annoyed me into writing my own simplified version
 ## plan
 
 1. took words from: https://github.com/derekchuank/high-frequency-vocabulary/
-2. extracted a subset to cover letters with equal frequency
-3. made frontend
+2. extracted a subset to cover letters with more equal frequency
+3. made the frontend
 4. pushed it to git pages
-5. looked into opportunities for errors/speed statistics per key using localstorage
+5. looked into opportunities for errors/speed statistics per key using local storage
--- words/rust-way/src/main.rs
+++ words/rust-way/src/main.rs
@@ -1,25 +1,81 @@
+use std::fs::File;
+use std::io::{Read, Write};
+
 use std::error::Error;
 type E = Box<dyn Error>;
 use regex::Regex;
 use std::collections::HashMap;
 
+// one easy available optimisation would be to
+// score only individual candidates
+// and not to recompute the whole set score
+// but current way it is just simpler
+
 fn main() -> Result<(), E> {
     let mut subset = load_words()?;
-    subset.retain(|x| x.len() > 3);
+
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    subset.retain(|x| x.len() == 5);
+    let group_size = 100;
+    let times = 100; // save after
 
     println!("{}", subset.len());
 
     let finders = letter_finders();
-    println!("score: {}", score(subset, finders));
+    println!("score: {}", score(&subset, &finders));
+
+    use rand::distributions::{Distribution, Uniform};
+
+    let mut rng = rand::thread_rng();
+
+
+    loop {
+        for _ in 0..times {
+            let between = Uniform::new(0, subset.len());
+
+            let mut group = vec![];
+            for _ in 0..group_size {
+                let index = between.sample(&mut rng);
+                group.push(Candidate { index });
+            }
+            let finalist = group.iter().min_by(|a, b| {
+                let mut copy = subset.to_vec();
+                copy.remove(a.index);
+                let score_a = score(&copy, &finders);
+
+                let mut copy = subset.to_vec();
+                copy.remove(b.index);
+                let score_b = score(&copy, &finders);
+
+                use std::cmp::Ordering::Equal;
+                score_a.partial_cmp(&score_b).unwrap_or(Equal)
+            }).unwrap();
+            subset.remove(finalist.index);
+
+            println!("score: {}", score(&subset, &finders));
+        }
+        let text = subset.join("\n");
+        println!("{}", subset.len());
+
+        let dir = "../../subsets/";
+        let name1 = [dir, "current"].concat();
+        let name2 = [dir, format!("subset-{}", subset.len()).as_str()].concat();
+        let mut file = File::create(name1)?;
+        file.write_all(text.as_bytes())?;
+        let mut file = File::create(name2)?;
+        file.write_all(text.as_bytes())?;
+    }
 
     Ok(())
 }
 
-fn load_string() -> Result<String, E> {
-    use std::fs::File;
-    use std::io::Read;
+struct Candidate {
+    index: usize,
+}
 
-    let mut file = File::open("../../10k.txt")?;
+fn load_string() -> Result<String, E> {
+    // let mut file = File::open("../../10k.txt")?;
+    let mut file = File::open("../../subsets/current")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     println!("{}", contents);
@@ -55,10 +111,10 @@ fn empty_counts() -> HashMap<String, usize> {
     result
 }
 
-fn score(words: Vec<String>, finders: HashMap<String, Regex>) -> f64 {
+fn score(words: &Vec<String>, finders: &HashMap<String, Regex>) -> f64 {
     let mut counts = empty_counts();
     for word in words {
-        for (letter, regex) in &finders {
+        for (letter, regex) in finders {
             let count = regex.find_iter(word.as_str()).count();
             let key = letter.to_string();
             let value = counts.get(&key).unwrap();
