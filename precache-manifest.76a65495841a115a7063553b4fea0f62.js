153914b6415f2fe76767bc50dbe226ee57924720
Alexander K <code.alexander.k@gmail.com>
2019-09-14 17:46:29 +0500

    Updates

---
+++ precache-manifest.76a65495841a115a7063553b4fea0f62.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08deff86add65643a1749477cb112919",
+    "url": "/typing-speed/index.html"
+  },
+  {
+    "revision": "bf63b1850aaa4a449345",
+    "url": "/typing-speed/static/js/2.91977de1.chunk.js"
+  },
+  {
+    "revision": "190b4d1bc0f214699f53",
+    "url": "/typing-speed/static/js/main.9654e9a3.chunk.js"
+  },
+  {
+    "revision": "ef72109ab765a58c9d48",
+    "url": "/typing-speed/static/js/runtime~main.9a82bc1a.js"
+  }
+]);
